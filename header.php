ec66f179fd75e861f78c77c9d8e696ead1c82e20
barryways <93161779+barryways@users.noreply.github.com>
2022-06-28 20:25:49 -0600

    Psuh del trabajo hecho hasta el 28/06, codigo html, css listo para modificiones

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEC-MARCAJES</title>
+    <!--Preloads para css-->
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="preload" href="../css/styles.css" as="style">
+    <link rel="stylesheet" href="../css/normalize.css" >
+    <link rel="preload" href="../css/normalize.css" as="style">
+    <!--gOOGLE FONTS-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400&display=swap" rel="stylesheet">
+</head>
+ 
+<header class="header">
+        <!--Imagen del logo-->
+        <img  class="logo-marcajes" src="../imgs/Marcajes-logo.png" alt="LOGO">
+        <!--Imagen del login-->
+        <a href="#" class="login">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="64" height="64" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <circle cx="12" cy="7" r="4" />
+                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+            </svg>
+        </a>
+    </header>
